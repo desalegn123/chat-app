28bb6eb32423e55db71fea280bba63481561edae
Desalegn <sisay963646@gmail.com>
2025-02-21 21:58:38 +0200

    full message commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='underline text-4xl text-green-500'>Hello Guys</h1>
+      
+    </div>
+  )
+}
+
+export default App
